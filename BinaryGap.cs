b19a11722307dbb89c21c5d03a77e71c353214ee
William Bunin <snowyglaze33@gmail.com>
2020-04-07 16:14:47 -0500

    Add BinaryGap.cs
    - Add BinaryGap.cs
    - Add TestBinaryGap() procedure

---
+++ BinaryGap.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    //Find longest sequence of zeros in binary representation of an integer.
+    class BinaryGap
+    {
+        public static int GetBinaryGap(int number)
+        {
+            if (number < 1)
+            {
+                throw new ArgumentOutOfRangeException("number", number, "number cannot be less than 1");
+            }
+
+            string numberAsBinary = GetBinary(number);
+
+            int maxBinaryGroup = 0;
+            int currentBinaryGroup = 0;
+
+            foreach (char digit in numberAsBinary)
+            {
+                if (digit.Equals('1'))
+                {
+                    if (maxBinaryGroup < currentBinaryGroup)
+                    {
+                        maxBinaryGroup = currentBinaryGroup;
+                    }
+
+                    currentBinaryGroup = 0;
+                }
+                else
+                {
+                    currentBinaryGroup++;
+                }
+            }
+
+            return maxBinaryGroup;
+        }
+
+        public static string GetBinary(int number)
+        {
+            StringBuilder binaryNumber = new StringBuilder(String.Empty);
+
+            int numberDivided = number / 2;
+
+            if (numberDivided > 1)
+            {
+                string previousDigits = GetBinary(numberDivided);
+                binaryNumber.Append(previousDigits);
+            }
+            else
+            {
+                binaryNumber.Append("1");
+            }
+
+            binaryNumber.Append(number % 2);
+
+            return binaryNumber.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,54 @@ namespace Codility
     {
         static void Main(string[] args)
         {
+            TestBinaryGap();
+        }
+
+        static void TestBinaryGap()
+        {
+            int testNumber = 1;
+            Console.WriteLine("Binary gap for " + testNumber + " is " + BinaryGap.GetBinaryGap(testNumber));
+
+            testNumber = int.MaxValue;
+            Console.WriteLine("Binary gap for " + testNumber + " is " + BinaryGap.GetBinaryGap(testNumber));
+
+            testNumber = -1;
+            try
+            {
+                Console.WriteLine("Binary gap for " + testNumber + " is " + BinaryGap.GetBinaryGap(testNumber));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("-1 is out of bounds and will therefore fail.");
+            }
+
+            do
+            {
+                Console.WriteLine("Type out an int you want to try.");
+
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out testNumber))
+                {
+
+                    try
+                    {
+                        Console.WriteLine("Binary gap for " + testNumber + " is " + BinaryGap.GetBinaryGap(testNumber));
+                    }
+                    catch (ArgumentOutOfRangeException ex)
+                    {
+                        Console.WriteLine(testNumber + " is out of bounds and will therefore fail.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(userInput + " cannot be converted to an int. Please type a valid int.");
+                }
+
+                Console.WriteLine("Type anything and press enter to exit the loop. Otherwise press enter to continue testing.");
+            } while (String.IsNullOrWhiteSpace(Console.ReadLine()))
+                    ;
+
         }
     }
 }
